c947c84be50fe11fa730a0c2c1f9fd5d414bc693
Robert Lange <robert.lange@sage.de>
2018-11-16 23:49:01 +0100

    add TableGateway and Model

---
+++ skeleton-application/module/Album/src/Model/Album.php
@@ -0,0 +1,25 @@
+<?php
+/*
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+*/
+/* 
+    Created on : 16.11.2018, 23:40:45
+    Author     : rlange
+*/
+namespace Album\Model;
+
+class Album
+{
+    public $id;
+    public $artist;
+    public $title;
+
+    public function exchangeArray(array $data)
+    {
+        $this->id     = !empty($data['id']) ? $data['id'] : null;
+        $this->artist = !empty($data['artist']) ? $data['artist'] : null;
+        $this->title  = !empty($data['title']) ? $data['title'] : null;
+    }
+}
